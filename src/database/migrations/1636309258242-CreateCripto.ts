33986585c8f9610dbc9a136e5d2962d282581af5
Fischer <fischerrobson@gmail.com>
2021-11-07 15:33:20 -0300

    base do projeto e tabela de criptos criada

---
+++ src/database/migrations/1636309258242-CreateCripto.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCripto1636309258242 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "Criptos",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar"
+          },
+          {
+            name: "initials",
+            type: "varchar"
+          },
+          {
+            name: "value",
+            type: "numeric"
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable("Criptos");
+  }
+
+}
